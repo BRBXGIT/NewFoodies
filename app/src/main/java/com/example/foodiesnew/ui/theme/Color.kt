2ea0d5b64ae6ca90caa5d6f5bcc2f689dd7e4568
BRBX <egorka2005sautkin@gmail.com>
2024-08-22 17:21:25 +0300

    set up theme

--- app/src/main/java/com/example/foodiesnew/ui/theme/Color.kt
+++ app/src/main/java/com/example/foodiesnew/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.example.foodiesnew.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PineGreen = Color(0xff264e35)
+val FullyWhite = Color(0xffffffff)
+val DarkCharcoal = Color(0xff272727)
+val CoralRed = Color(0xffff4d4c)
+val Alabaster = Color(0xfff4f4f4)
+val SlateGray = Color(0xff5f5f5f)
+val Tangerine = Color(0xffee7002)
